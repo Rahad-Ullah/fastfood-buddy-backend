34a7ee905e15a07c120ebf669e07fa4ae17419ea
Rahad Ullah <rahadullah10@gmail.com>
2025-12-25 18:39:42 +0600

    fix(user.validationt.s): user validation on updating profile

--- src/app/modules/user/user.validation.ts
+++ src/app/modules/user/user.validation.ts
@@ -2,18 +2,25 @@ import { z } from 'zod';
 import { USER_STATUS } from './user.constant';
 
 const createUserZodSchema = z.object({
-  body: z.object({
-    name: z.string({ required_error: 'Name is required' }),
-    email: z.string({ required_error: 'Email is required' }),
-    password: z.string({ required_error: 'Password is required' }),
-  }),
+  body: z
+    .object({
+      name: z.string({ required_error: 'Name is required' }),
+      email: z.string({ required_error: 'Email is required' }),
+      password: z.string({ required_error: 'Password is required' }),
+    })
+    .strict(),
 });
 
 const updateUserZodSchema = z.object({
-  name: z.string().optional(),
-  image: z.string().optional(),
-  status: z.nativeEnum(USER_STATUS).optional(),
+  body: z
+    .object({
+      name: z.string().optional(),
+      image: z.string().optional(),
+      status: z.nativeEnum(USER_STATUS).optional(),
+    })
+    .strict(),
 });
+  
 
 export const UserValidation = {
   createUserZodSchema,
